30fb75f0683c0eb1e7faf83ca8baabddc11f12b4
see-sharp-dev <>
2025-10-26 00:00:44 +0200

    initial commit

---
+++ src/server/db/schema.ts
@@ -0,0 +1,24 @@
+import { sql } from "drizzle-orm";
+import { index, sqliteTableCreator } from "drizzle-orm/sqlite-core";
+
+/**
+ * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
+ * database instance for multiple projects.
+ *
+ * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
+ */
+export const createTable = sqliteTableCreator((name) => `trips_${name}`);
+
+export const posts = createTable(
+  "post",
+  (d) => ({
+    id: d.integer({ mode: "number" }).primaryKey({ autoIncrement: true }),
+    name: d.text({ length: 256 }),
+    createdAt: d
+      .integer({ mode: "timestamp" })
+      .default(sql`(unixepoch())`)
+      .notNull(),
+    updatedAt: d.integer({ mode: "timestamp" }).$onUpdate(() => new Date()),
+  }),
+  (t) => [index("name_idx").on(t.name)],
+);
