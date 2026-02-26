ea980ebb62f059769e50b770b813419704017da5
see-sharp-dev <>
2026-02-26 20:31:39 +0100

    feat: add PDF export functionality to itinerary (#101)

---
+++ src/lib/pdf/index.ts
@@ -0,0 +1,10 @@
+/**
+ * PDF generation module exports
+ */
+
+// Main PDF generation functions
+export {
+  generateAllDaysPdf,
+  generateDayPdf,
+  type PdfProgressCallback,
+} from "./generate-day-pdf";
