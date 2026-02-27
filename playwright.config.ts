b1d8d0acea35d462a25e46474d496f2075a1bc2c
see-sharp-dev <>
2026-02-27 11:23:59 +0100

    fixes

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,13 +8,14 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? "github" : "html",
+  reporter: process.env.CI ? [["github"], ["html", { open: "never" }]] : "html",
   expect: {
     timeout: 60000,
   },
   use: {
     baseURL,
     trace: "on-first-retry",
+    screenshot: "only-on-failure",
   },
   projects: [
     {
